20d55db70ad3e7d20f75371588b6fb7adb68040f
Zunilda-Niveiro <niveiro.zunilda@gmail.com>
2022-07-31 17:10:39 -0300

    creando login

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ const path = require('path');
 app.use(express.static('public'));
 app.get('/', (req,res)=> res.sendFile(path.join(__dirname,"views","home.html")));
 app.get('/register', (req,res)=> res.sendFile(path.join(__dirname,"views","register.html")));
+app.get('/login', (req,res)=> res.sendFile(path.join(__dirname,"views","login.html")));
 
 app.listen(3030, ()=> console.log("servidor en linea"))
